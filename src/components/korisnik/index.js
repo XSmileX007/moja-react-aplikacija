7042989ffb888e1c197d65e536035858a5b72b5b
Goran Marjanovic <goran.marjanovic@atos.net>
2023-05-04 21:25:04 +0200

    added liste

--- src/components/korisnik/index.js
+++ src/components/korisnik/index.js
@@ -1,3 +1,5 @@
 export { default as KorisnikChildren } from './KorisnikChildren';
 export { default as KorisnikKlasa } from './KorisnikKlasa';
 export { default as KorisnikFunction } from './KorisnikFunction';
+
+export { default as KorisnikKomponenta } from './KorisnikKomponenta';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-import App from './components/App';
+
+// zadatak liste ucitavamo iz AppListe.js
+import App from './components/AppListe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
